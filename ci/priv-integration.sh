81e617fe746dc03e577e7b6146ec5a922950bfeb
Colin Walters <walters@verbum.org>
2023-01-08 11:26:06 -0500

    ci/priv-integration: Stop testing compat image
    This isn't necessary anymore, and further we need this
    in order for the counts to be right for other patches backported.
    
    See commit 2bfac370aa52ba8a0e5089aee74924861671e401

--- ci/priv-integration.sh
+++ ci/priv-integration.sh
@@ -7,8 +7,6 @@ set -euo pipefail
 sysroot=/run/host
 # Current stable image fixture
 image=quay.io/fedora/fedora-coreos:testing-devel
-# An unchunked v1 image
-old_image=quay.io/cgwalters/fcos:unchunked
 imgref=ostree-unverified-registry:${image}
 stateroot=testos
 
@@ -26,7 +24,7 @@ ostree-ext-cli container image deploy --sysroot "${sysroot}" \
 ostree admin --sysroot="${sysroot}" status
 ostree-ext-cli container image remove --repo "${sysroot}/ostree/repo" registry:"${image}"
 ostree admin --sysroot="${sysroot}" undeploy 0
-for img in "${image}" "${old_image}"; do
+for img in "${image}"; do
     ostree-ext-cli container image deploy --sysroot "${sysroot}" \
         --stateroot "${stateroot}" --imgref ostree-unverified-registry:"${img}"
     ostree admin --sysroot="${sysroot}" status
